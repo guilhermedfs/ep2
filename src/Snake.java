8c423b11ec348e8c55c03da80c1bbfe029d1a5ba
Guilherme <guilhermedfs11@gmail.com>
2019-11-07 23:23:36 -0300

    Now Snakes have different classes and Canvas can be changed.

---
+++ src/Snake.java
@@ -0,0 +1,14 @@
+
+public class Snake {
+	private static int length = 3;
+
+	public static int getLength() {
+		return length;
+	}
+
+	public static void setLength(int length) {
+		Snake.length = length;
+	}
+	
+
+}
